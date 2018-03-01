44f17197639a7757a7632067472d13a3cc5b129e
Marina <35458264+marishahv@users.noreply.github.com>
2018-03-01 17:36:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var bracketsObj = {};    
+
+  for(var i = 0, len = bracketsConfig.length; i < len; i++){
+      bracketsObj[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  function parseStr(string){
+      var stackOfbrackets = [];
+      var i = 0;
+      var j = 0;
+
+      if(!(string.charAt(0) in bracketsObj))
+          return false;   
+     
+      while(i < string.length){
+          if(stackOfbrackets.length == 0){
+              stackOfbrackets.push(string.charAt(i)); 
+              i++;  
+              j++; 
+          }            
+          if(validateBrackets(stackOfbrackets[j-1], string.charAt(i))){
+              stackOfbrackets.pop();
+              i++;
+              j--;
+          }
+          else{
+              stackOfbrackets.push(string.charAt(i));
+              i++;
+              j++;
+          }            
+      }
+      return stackOfbrackets.length == 0 ? true : false;
+  }
+  
+  function validateBrackets(bracketOpen, bracketClosed){
+      if(bracketOpen in bracketsObj){
+          return bracketsObj[bracketOpen] === bracketClosed;
+      }
+      else
+          return false;
+  }
+  
+  return parseStr(str);    
 }
+
